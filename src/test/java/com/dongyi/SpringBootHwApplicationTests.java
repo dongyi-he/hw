3ccd4bd9cc712384831c1be5bb05e7980f16be1e
Dongyi He <dongyi_he@hotmail.com>
2020-02-03 19:08:16 +0800

    Initial commit

---
+++ src/test/java/com/dongyi/SpringBootHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
